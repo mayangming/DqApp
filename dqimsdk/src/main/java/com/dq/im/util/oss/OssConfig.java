b41d74074a13c22496b47eba5dc0ef37c8bc410f
18310400074 <mabiao123>
2020-06-16 13:52:49 +0800

    修改oss获取token地址

--- dqimsdk/src/main/java/com/dq/im/util/oss/OssConfig.java
+++ dqimsdk/src/main/java/com/dq/im/util/oss/OssConfig.java
@@ -7,6 +7,6 @@ import com.dq.im.constants.URLUtil;
  */
 public class OssConfig {
     public static String endpoint = "https://oss-cn-beijing.aliyuncs.com";
-    public static String stsServer = URLUtil.getServer()+"Test/appTokenServer";
+    public static String stsServer = URLUtil.getServer()+"oss/appTokenServer";
     public static String bucketName = "dq-oss";
 }
